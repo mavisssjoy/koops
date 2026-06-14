b83add7dde52b55b179af0591e1da59323b7769a
mavisssjoy <naufalnobe12@gmail.com>
2026-06-14 10:10:28 +0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = 'https://rvgwbakqagmywppqeqyn.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_NOi3piRzUy8TmX7oDENjng_GMT_zHIg';
-
-const { createClient } = supabase;
-const db = createClient(SUPABASE_URL, SUPABASE_KEY);
